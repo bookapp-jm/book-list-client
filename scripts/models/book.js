5f3a51a0c99b2c7cbbd91137d2a65e4f4a332164
Mitchell M <mitchellm1618@gmail.com>
2017-12-13 09:25:23 -0800

    revisions for functionality

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -1,38 +1,33 @@
 'use strict';
 
-function Book(rawDataObj) {
-    Object.keys(rawDataObj).forEach(key => {
-        this[key] = rawDataObj[key];
-    }, this);
-}
+let app = app || {};
+let __API_URL__ = 'hptt://localhost:3000'; //can also make it the deployed version which would be heroku_
 
-Book.prototype.toHtml = function () {
-    var template = Handlebars.compile($('#book-list-template').text());
+(function(module) {
+  function errorCallback(err) {
+    console.error(err);
+    module.errorView.initErrorPage(err);
+  }
+
+  function Book(rawDataObj) {
+    Object.keys(rawDataObj).forEach(key => this[key] = rawDataObj[key]);
+  }
+
+  Book.prototype.toHtml = function () {
+    let template = Handlebars.compile($('#book-list-template').text());
     return template(this);
-};
+  };
 
-Book.all = [];
+  Book.all = [];
 
-Book.loadAll = rows => {
-    rows.sort((a, b) => b.title - a.title);
-    Book.all = rows.map(bookObject => new Book(bookObject));
+  Book.loadAll = rows => Book.all = rows.sort((a, b) => b.title - a.title).map(bookObject => new Book(bookObject));
 
-};
-
-Book.fetchAll = callback => {
-    $.get('/api/v1/books')
-        .then(results => {
-            Book.loadAll(results);
-        })
-        .catch(err => {
-            console.error(err);
-            errorCallback();
-        });
-};
-
-Define a static method on Book called loadAll which takes rows as an argument, and sorts rows by title, maps over rows to create an array of Book instances, and then assigns the new array of Books to Book.all.
-
-Define a static method on Book called fetchAll which takes callback as an argument, and makes a request to the API at GET: /api/v1 / books.
-On success, pass the results to Book.loadAll, and then invoke the callback.
-On failure, invoke the errorCallback.
+  Book.fetchAll = callback => {
+    $.get(`${__API_URL__}/api/v1/books`)
+      .then(data => Book.loadAll(data))
+      .then(callback)
+      .catch(err => errorCallback(err));
+  };
+  module.Book = Book;
+})(app);
 
