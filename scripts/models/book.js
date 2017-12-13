073bee7df22d2633ce68e0c92eb4dd57455587e6
Mitchell M <mitchellm1618@gmail.com>
2017-12-13 11:35:34 -0800

    changes to index.html

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -13,20 +13,34 @@ let __API_URL__ = 'hptt://localhost:3000'; //can also make it the deployed versi
     Object.keys(rawDataObj).forEach(key => this[key] = rawDataObj[key]);
   }
 
-  Book.prototype.toHtml = function () {
+  Book.toHtml = function () {
     let template = Handlebars.compile($('#book-list-template').text());
     return template(this);
   };
 
   Book.all = [];
-
   Book.loadAll = rows => Book.all = rows.sort((a, b) => b.title - a.title).map(bookObject => new Book(bookObject));
+
+  Book.fetchOne = function () {
+    $.get(`${__API_URL__}/api/v1/book/:id`)
+      .then(data => Book.loadAll(data))
+      .then(callback)
+      .catch(err => errorCallback(err));
+  };
 
   Book.fetchAll = callback => {
     $.get(`${__API_URL__}/api/v1/books`)
       .then(data => Book.loadAll(data))
       .then(callback)
       .catch(err => errorCallback(err));
+  };
+
+  Book.prototype.create = function (callback) {
+    $.post('/articles', { author: this.author, authorUrl: this.authorUrl, body: this.body, category: this.category, publishedOn: this.publishedOn, title: this.title })
+      .then(data => {
+        console.log(data);
+        if (callback) callback();
+      })
   };
   module.Book = Book;
 })(app);
