440aeed58c561c64a629b6cd297def25b1433c59
Mitchell M <mitchellm1618@gmail.com>
2017-12-17 19:59:20 -0800

    added a bunch of functionality and admin page

--- scripts/views/book-view.js
+++ scripts/views/book-view.js
@@ -5,14 +5,14 @@ var app = app || {};
 
 (function(module) {
   $('.icon-menu').on('click', function(e) {
-    $('.nav-menu').slideDown(750);
+    $('.nav-links').slideDown(750);
   });
 
   const bookView = {};
 
   bookView.initIndexPage = function(ctx) {
     $('.container').hide();
-    $('.nav-menu').slideDown(750);
+    $('.nav-links').slideDown(750);
     $('.books-view').show();
     $('#book-list').empty();
     app.Book.all.map(book => $('#book-list').append(book.toHtml()));
@@ -21,16 +21,25 @@ var app = app || {};
 
   bookView.initDetailPage = function(ctx) {
     $('.container').hide();
-    $('.nav-menu').slideDown(750);
+    $('.nav-links').slideDown(750);
     $('.book-view').show(); //shows the book view container
     $('#book-detail').empty(); //to clear out any prior entries
     let template = Handlebars.compile($('#book-detail-template').text()); //compiles the detail template with given information as text
     $('#book-detail').append(template(ctx)); //appends the template with given contextual item
+
+    $('#update-button').on('click', function() {
+      page(`/books/${$(this).data('book_id')}/update`);
+    });
+
+    $('#delete-button').on('click', function () {
+      app.Book.destroy($(this).data('book_id'));
+    });
+    // next();
   };
 
   bookView.initFormPage = function() {
     $('.container').hide();
-    $('.nav-menu').slideDown(750);
+    $('.nav-links').slideDown(750);
     $('.create-book').show();
     $('#new-book').on('submit', function(e){
       e.preventDefault();
@@ -44,6 +53,32 @@ var app = app || {};
       };
       console.log('book', book);
       app.Book.create(book);
+    });
+  };
+
+  bookView.initUpdateFormPage = function (ctx) {
+    $('.container').hide();
+    $('.nav-links').slideDown(750);
+    $('.update-view').show();
+    $('.update-form input[name="title"]').val(ctx.book.title);
+    $('.update-form input[name="author"]').val(ctx.book.author);
+    $('.update-form input[name="isbn"]').val(ctx.book.isnb);
+    $('.update-form input[name="image_url"]').val(ctx.book.image_url);
+    $('.update-form textarea[name="description"]').val(ctx.book.description);
+
+    $('.update-form').on('submit', function(e) {
+      e.preventDefault();
+
+      let book = {
+        book_id: ctx.book.book_id,
+        title: e.target.title.value,
+        author: e.target.author.value,
+        isbn: e.target.isbn.value,
+        image_url: e.target.image_url.value,
+        description: e.target.description.value,
+      };
+
+      module.Book.update(book, book.book_id);
     });
   };
 
--- scripts/views/routes.js
+++ scripts/views/routes.js
@@ -1,12 +1,16 @@
-// import { URL } from "url";
-
 'use strict';
 
-page('/', () => app.Book.fetchAll(app.bookView.initIndexPage));
-page('/books/new', ctx => app.bookView.initFormPage(ctx));
-page('/books/:book_id', ctx => app.Book.fetchOne(ctx, app.bookView.initDetailPage));
+page('/', () => app.Book.fetchAll(app.bookView.initIndexPage)), (ctx, next) => app.adminView.verify(ctx, next);
 
-page('/*', () => app.Book.fetchAll(app.bookView.initIndexPage));
+page('/books/new', ctx => app.bookView.initFormPage(ctx));
+
+page('/books/:book_id', ctx => app.Book.fetchOne(ctx, app.bookView.initDetailPage)), (ctx, next) => app.adminView.verify(ctx, next);
+
+page('/admin', ctx => app.adminView.initAdminPage(ctx));
+
+page('/books/:book_id/update', (ctx, next) => app.Book.fetchOne(ctx, next), ctx => app.bookView.initUpdateFormPage(ctx));
+
+// page('/*', () => app.Book.fetchAll(app.bookView.initIndexPage));
 
 // app.get('*', (request, response) => response.redirect(CLIENT_URL));
 
